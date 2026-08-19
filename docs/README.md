5407e079054f2f3e7f8aba3048d7854fa4560db1
Teoh Yi Le <yileteoh@gmail.com>
2026-08-19 18:01:29 +0800

    Rename chatbot from Duke to Bot67

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bot67 User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bot67.java
@@ -0,0 +1,10 @@
+public class Bot67 {
+    public static void main(String[] args) {
+        String banner =  "_____       ___    __  ______\n"
+                + "| __ )  ___ | |_  / /  |___  |\n"
+                + "|  _ \\ / _ \\| __|/ /_     / /\n"
+                + "| |_) | (_) | |_| '_ \\   / /\n"
+                + "|____/ \\___/ \\__|\\___/  /_/\n";
+        System.out.println(banner);
+    }
+}
